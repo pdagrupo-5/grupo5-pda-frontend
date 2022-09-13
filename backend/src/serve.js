f367e99252680be5a0c27f6db4684aaae2204f28
vitorxxd3 <lulubr38@gmail.com>
2022-09-13 18:17:34 -0300

    organizacao das pastas

---
+++ backend/src/serve.js
@@ -0,0 +1,21 @@
+require('dotenv/config');
+const express = require("express");
+const usuarioRoute = require('../src/routes/routes');
+const funcionarioRoute = require('../src/routes/funcionario.routes');
+const veiculoRoute = require('../src/routes/veiculo.routes');
+const estadoRoute = require('../src/routes/estado.routes');
+const cors = require("cors");
+const app = express();
+const port = 3200;
+
+
+app.use(express.json());
+app.use(cors())
+app.use(usuarioRoute)
+app.use(veiculoRoute)
+app.use(funcionarioRoute)
+app.use(estadoRoute)
+
+app.listen(port ,() => {
+    console.log(`servidor iniciado na porta ${port}`) 
+   });
